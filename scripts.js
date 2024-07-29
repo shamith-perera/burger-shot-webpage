97e305baba2f6748f881de740d2ee088703b7128
shamith-perera <shamith.gsjp@gmail.com>
2024-07-29 10:22:52 +0530

    finished basic structure

--- scripts.js
+++ scripts.js
@@ -1,9 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(function () {
+document.addEventListener('DOMContentLoaded', function() {
+    
+    document.body.classList.add('no-scroll');
+
+    
+    setTimeout(function() {
         const loadingScreen = document.getElementById('loading-screen');
+
+       
         loadingScreen.classList.add('break-animation');
-        setTimeout(function () {
+
+      
+        setTimeout(function() {
             loadingScreen.classList.add('hidden');
-        }, 1000);
-    }, 1500);
+          
+            document.body.classList.remove('no-scroll');
+        }, 1000); 
+    }, 1000);
+});
+
+
+document.getElementById('nav-bar-icon').addEventListener('click', function() {
+    location.reload();
 });
